e7435ed3411075521eb9ed10dffccf3f7c8ff407
Olivier Van Aken <olivier.va@outlook.com>
2019-10-08 19:49:54 +0100

    replaced separate stdout and stderr scanners with using the os stdout and stderr

--- internal/runner/commands.go
+++ internal/runner/commands.go
@@ -1,12 +1,10 @@
 package runner
 
 import (
-	"bufio"
-	"fmt"
 	"github.com/restechnica/taskforce/internal/config"
 	"github.com/restechnica/taskforce/internal/extensions/osext"
 	"github.com/restechnica/taskforce/internal/extensions/shellext"
-	"io"
+	"os"
 	"os/exec"
 )
 
@@ -18,40 +16,14 @@ func RunCommand(command config.Command) (err error) {
 		return
 	}
 
-	var execution = exec.Command(executable, arguments...)
+	var process = exec.Command(executable, arguments...)
 
 	if command.HasDirectory() {
-		execution.Dir = osext.ExpandTilde(command.Directory)
+		process.Dir = osext.ExpandTilde(command.Directory)
 	}
 
-	var stdout, stderr io.ReadCloser
+	process.Stdout = os.Stdout
+	process.Stderr = os.Stderr
 
-	if stdout, err = execution.StdoutPipe(); err != nil {
-		return
-	}
-
-	if stderr, err = execution.StderrPipe(); err != nil {
-		return
-	}
-
-	var stdoutScanner, stderrScanner = bufio.NewScanner(stdout), bufio.NewScanner(stderr)
-
-	go printScanner(stdoutScanner)
-	go printScanner(stderrScanner)
-
-	if err = execution.Start(); err != nil {
-		return
-	}
-
-	if err = execution.Wait(); err != nil {
-		return
-	}
-
-	return
-}
-
-func printScanner(scanner *bufio.Scanner) {
-	for scanner.Scan() {
-		fmt.Println(scanner.Text())
-	}
+	return process.Run()
 }
