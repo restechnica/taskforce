07c4cf33f17882275d9cf6c887d0bb3add63376d
Olivier Van Aken <olivier.va@outlook.com>
2019-10-11 22:28:58 +0100

    added extra prints for debugging shell split bug on windows

--- internal/extensions/shellext/splitter.go
+++ internal/extensions/shellext/splitter.go
@@ -1,16 +1,23 @@
 package shellext
 
 import (
+	"fmt"
 	"github.com/kballard/go-shellquote"
+	"strings"
 )
 
 func SplitShellCommand(command string) (executable string, arguments []string, err error) {
-	var split []string
+	var fields = strings.Fields(command)
 
-	if split, err = shellquote.Split(command); err != nil {
+	var test []string
+
+	if test, err = shellquote.Split(command); err != nil {
 		return
 	}
 
-	executable, arguments = split[0], split[1:]
-	return
+	fmt.Println(test)
+
+	fmt.Println(fields)
+
+	return fields[0], fields[1:], err
 }
--- taskforce.hcl
+++ taskforce.hcl
@@ -10,6 +10,12 @@ command install {
   expression = "go install github.com/restechnica/taskforce/cmd/taskforce"
 }
 
-command install {
-  expression = "go install github.com/restechnica/taskforce/cmd/"
+command test {
+  expression = <<EOF
+    aws cloudformation deploy
+      --template-file some-template-file
+      --role-arn some-arn
+      --stack-name ai-papi
+      --parameter-overrides some_key=some_value some_key_some=some_value_some
+  EOF
 }
