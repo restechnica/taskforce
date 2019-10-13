ecc71f70c77e54efe0f7b18097b82da47fbf9b69
Olivier Van Aken <olivier.va@outlook.com>
2019-10-13 22:20:13 +0100

    abstracted dotenv logic

--- cmd/taskforce/main.go
+++ cmd/taskforce/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"github.com/joho/godotenv"
 	"github.com/restechnica/taskforce/internal/config"
+	"github.com/restechnica/taskforce/internal/environment"
 	"github.com/restechnica/taskforce/internal/execution"
 	"github.com/restechnica/taskforce/internal/hcl"
 	"log"
@@ -10,15 +10,12 @@ import (
 	"path"
 )
 
-const hclFileName = "taskforce.hcl"
-const dotenvFileName = ".env"
-
 func main() {
 	var err error
 	var configuration config.Root
 	var workingDirectory string
 
-	if err = godotenv.Load(dotenvFileName); err != nil {
+	if err = environment.Load("./.env"); err != nil {
 		log.Println(err)
 		log.Println("Failed to load .env file, proceeding without .env variables")
 	}
@@ -27,7 +24,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	var filePath = path.Join(workingDirectory, hclFileName)
+	var filePath = path.Join(workingDirectory, "taskforce.hcl")
 
 	if configuration, err = hcl.LoadHCLFile(filePath); err != nil {
 		log.Fatal(err)
